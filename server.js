c0069947585c28745e1f502d2a9eb7de97479dbe
gouravameta71@gmail.com <gouravameta71@gmail.com>
2023-05-23 10:41:35 +0530

    mongoose version updated

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-import {app} from "./app.js"
+import { app } from "./app.js";
 import { connectDB } from "./data/db.js";
-
 
 connectDB();
 
-
 app.listen(process.env.PORT, () => {
-  console.log(`Server is Working on Port : ${process.env.PORT} in ${process.env.NODE_ENV} Mode`);
+  console.log(
+    `Server is Working on Port : ${process.env.PORT} in ${process.env.NODE_ENV} Mode`
+  );
 });
