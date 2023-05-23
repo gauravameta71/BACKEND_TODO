c32dcf837dbd368ea0d0a3956daf850cff4e4593
gouravameta71@gmail.com <gouravameta71@gmail.com>
2023-05-23 11:46:05 +0530

    some changes

--- data/db.js
+++ data/db.js
@@ -6,6 +6,6 @@ export const connectDB = ()=>{
     .connect(process.env.MONGO_URI , {
       dbName: "BACKEND_API",
     })
-    .then(() => console.log("Database Connect"))
+    .then((c) => console.log(`Database Connected with ${c.connection.host}`))
     .catch((e) => console.log(e));
 }
